1adae07e1d503ab2cdfa66b7b17e126046f77245
GArosy <837225481@qq.com>
2022-08-16 16:42:20 +0800

    修改数据库配置：允许执行多条查询语句

--- db/connect/config.js
+++ db/connect/config.js
@@ -4,6 +4,7 @@ config = {
   user: "root", // 使用 root 用户登入 MySQL
   password: "Wxr02070.", // MySQL 密码
   database: "node-manage", // 指定使用的数据库
+  multipleStatements: true // 允许执行多条查询语句（默认关闭）
 };
 
 module.exports = config;
