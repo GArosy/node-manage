d064e002979837af63fef0da034581017ecc5b8b
GArosy <837225481@qq.com>
2022-09-13 19:53:10 +0800

    菜单接口

--- routes/user.js
+++ routes/user.js
@@ -1,51 +1,97 @@
 const { Router } = require("express");
-const jwt = require('jsonwebtoken')
-const fs = require("fs");
-const path = require("path");
+const jwt = require("../utils/jwt");
 const db = require("../db/connect/db");
 const logger = require("../utils/logger");
+const menu = require('../db/menuData/admin.json')
 
 const router = Router();
 const database = "userinfo";
-const host = "localhost";
-
 
 /**
  * 用户注册
  */
-router.post('/register', (req, res)=>{
-  let {userName, password} = req.query;
-  let searchSql = `select * from ${database} where userName='${userName}'`
-  let addSql = `insert into ${database} values '${userName}', '${password}'`;
-  db.queryDB(searchSql, (err, data)=>{
+router.post("/register", (req, res) => {
+  let { userName, password } = req.query;
+  let code = -1;
+  let searchSql = `select * from ${database} where userName='${userName}'`;
+  let addSql = `insert into ${database} values ('${userName}', '${password}')`;
+
+  // 打印日志
+  logger.info(
+    `[${req.method}-${res.statusMessage}-${req.originalUrl}-${
+      req.ip
+    }]: 用户注册:${JSON.stringify(req.query)}`
+  );
+
+  db.queryDB(searchSql, (err, data) => {
     if (err) {
       console.log(err);
       res.send(`query error: ${err}`);
       return;
     } else {
+      console.log(data);
       // 用户名是否已存在
-      if (data=[]) {
-        res.json({
-          code: "1",
+      if (data.length === 0) {
+        db.queryDB(addSql, (err) => {
+          if (err) {
+            console.log(err);
+            res.json({
+              code,
+            });
+            return;
+          } else {
+            code = 1;
+            res.json({
+              code,
+            });
+            return;
+          }
         });
       } else {
+        code = 0;
         res.json({
-          code: "0",
+          code,
         });
+        return;
       }
     }
-  })
-})
+  });
+});
 
 /**
  * 用户登录
  */
-router.post('/login', (req, res)=>{
-  let {userName, password} = req.body;
-  let sql = `select * from ${database} where username=${userName} and password=${password}`;
-  let arr = [userName, password]
+router.post("/login", (req, res) => {
+  let { userName, password } = req.query;
+  let sql = `select * from ${database} where username='${userName}' and password='${password}'`;
 
+  // 打印日志
+  logger.info(
+    `[${req.method}-${res.statusMessage}-${req.originalUrl}-${
+      req.ip
+    }]: 用户登录:${JSON.stringify(req.query)}`
+  );
 
-})
+  db.queryDB(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (data.length === 0) {
+        res.status(401).json({});
+      } else {
+        // 生成token
+        let payload = { username: data[0].username };
+        let token = jwt.encrypt(
+          payload,
+          60 * 10 // 签署10min令牌期限
+        );
+        res.status(200).json({
+          data: token,
+          menu
+        });
+      }
+    }
+  });
+});
 
-module.exports = router
+module.exports = router;
