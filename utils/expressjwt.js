78ed8ac8f83ecadfc64dffccb8b0595213e30b0b
GArosy <837225481@qq.com>
2022-09-14 13:35:05 +0800

    jwt工具封装

---
+++ utils/expressjwt.js
@@ -0,0 +1,16 @@
+const { expressjwt } = require("express-jwt");
+const secret = require("./jwtsecret");
+
+module.exports =  expressjwt({
+  // 加密时所用的密匙
+  secret: secret.jwtSecret,
+  // 设置算法
+  algorithms: ["HS256"],
+  // 无token请求不进行解析，并且抛出异常
+  credentialsRequired: false,
+}).unless({
+  // 白名单
+  path: ["/api/user/login", "/api/user/register"],
+});
+
+
--- utils/jwt.js
+++ utils/jwt.js
@@ -11,20 +11,28 @@ const Token = {
       { expiresIn: time } // 签署time s令牌期限
     );
   },
-  // 解密函数
+  // 验证中间件
   // jwt.verify(token,str) 参数token表示需要解密的令牌，str为密钥
-  decrypt(token) {
-    try {
-      let data = jwt.verify(token, secret.jwtSecret);
-      return {
-        token: true,
-        data,
-      };
-    } catch (error) {
-      return {
-        token: false,
-        data: error,
-      };
+  decrypt(req, res, next) {
+    let token = req.headers.authorization;
+    token = token ? token.split(" ")[1] : null;
+    if (!token) {
+      return res.status(401).end();
+    } else {
+      try {
+        let data = jwt.verify(token, secret.jwtSecret);
+        req.auth = data;
+        next();
+      } catch (error) {
+        console.log(error);
+        if (error.name === "UnauthorizedError") {
+          return res.status(401).send("invalid token...");
+        } else if (error.name === "TokenExpiredError") {
+          return res.status(401).send("token expired...");
+        }else{
+          next(error);
+        }
+      }
     }
   },
 };
